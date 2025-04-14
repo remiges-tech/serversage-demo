7994ec8f996e7b4266928e12a405a3d5acb0d933
Aniket Shinde <aniket.shinde@remiges.tech>
2025-04-14 13:31:36 +0530

    updates k6 script- api calls

--- etc/k6-script.js
+++ etc/k6-script.js
@@ -3,8 +3,8 @@ import { sleep } from 'k6';
 
 export default function () {
 
-  var server_list = ["localhost:8000", "localhost:8081", "localhost:3001"]
-  var endpoint_list = ["/", "/io_task", "/cpu_task", "/random_sleep", "/random_status", "/chain", "/error_test"]
+  var server_list = ["localhost:8081"]
+  var endpoint_list = ["/init", "/order","/order/{id}","order_list","/total_order","/rejected_order","/cpu_task", "/random_sleep", "/random_status", "/chain", "/error_test"]
 
   // Additional API calls on port 8081
   var additionalEndpoints = [
@@ -47,7 +47,19 @@ export default function () {
   server_list.forEach(function(server) {
     endpoint_list.forEach(function(endpoint) {
       http.get("http://" + server + endpoint);
+      if (endpoint == "/order" ) {
+        http.put("http://" + server + endpoint);
+        if (getRandom(1, 20) < 10 ){
+          http.put("http://" + server + endpoint);
+          http.del("http://" + server + endpoint);
+        }
+        http.post("http://" + server + endpoint);
+      }
     });
   });
   sleep(0.2);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
